4a7b02f013c71f9394fd9c402524d728a8730270
dimapod <Sonrisa50>
2014-01-03 10:07:08 +0100

    Migration to AngularFire 0.5.0

--- app/js/app.js
+++ app/js/app.js
@@ -24,5 +24,20 @@ angular.module('yawl').constant('FBURL', 'https://[firebase name].firebaseio.com
 // Authentication
 angular.module('yawl').run(['$rootScope', 'FireRef',
     function ($rootScope, FireRef) {
-        // -- initialise angularFireAuth here --
+
+        // -- initialise $firebaseAuth here --
+
+
+        // Tweak: Manage the case when user goes directly to "/login" and has to be redirected to "/" after sign-in
+        var redirect;
+        $rootScope.$on("$routeChangeStart", function (e, next) {
+            if (!redirect) redirect = next.originalPath;
+        });
+        $rootScope.$on('$firebaseAuth:login', function () {
+            if (redirect == "/login") {
+                $location.replace();
+                $location.path("/");
+                redirect = undefined;
+            }
+        });
     }]);
--- app/js/controllers/wishlistsCtrl.js
+++ app/js/controllers/wishlistsCtrl.js
@@ -3,7 +3,7 @@
 angular.module('yawl.controllers.wishlists', []).
     controller('wishlistsCtrl', ['$scope', 'wishlistCollection', function ($scope, wishlistCollection) {
         var self = this;
-        $scope.$on('angularFireAuth:login', function () {
+        $scope.$on('$firebaseAuth:login', function () {
             self.getWishlistCollection();
         });
 
